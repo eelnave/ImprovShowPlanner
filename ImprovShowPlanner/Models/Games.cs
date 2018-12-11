08b7c10617097696c917f834f56da7d68beb3f22
Natecus <natecus12@gmail.com>
2018-12-11 09:39:21 -0700

    * Games.cs: Added Games model
    * IndivShow.cs: added model for idividual shows

---
+++ ImprovShowPlanner/Models/Games.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace ImprovShowPlanner.Models
+{
+    public class Games
+    {
+            public int gamesID { get; set; }
+            public string name { get; set; }
+            public int numPlayers { get; set; }
+            public string desc { get; set; }
+            public string type { get; set; }
+
+        public Games()
+        {
+
+        }
+    }
+}
---
+++ ImprovShowPlanner/Models/IndivShow.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace ImprovShowPlanner.Models
+{
+    public class IndivShow
+    {
+        public int showId { get; set;}
+        public DateTime date { get; set; }
+
+        public IndivShow()
+        {
+        }
+    }
+}
