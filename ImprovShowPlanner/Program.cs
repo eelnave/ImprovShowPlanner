724734e540e6c74a559e3a74a32bfabbda886920
Nate <Nate@>
2018-12-18 11:59:48 -0800

    added a Initializer for the Database

--- ImprovShowPlanner/Program.cs
+++ ImprovShowPlanner/Program.cs
@@ -4,9 +4,12 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using ImprovShowPlanner.Data;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace ImprovShowPlanner
 {
@@ -14,7 +17,22 @@ namespace ImprovShowPlanner
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+            using(var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<ImprovShowContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch(Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occured initializing the Database");
+                }
+            }
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
--- ImprovShowPlanner/Startup.cs
+++ ImprovShowPlanner/Startup.cs
@@ -61,7 +61,7 @@ namespace ImprovShowPlanner
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Players}/{action=Index}/{id?}");
             });
         }
     }
